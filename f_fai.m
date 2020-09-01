7a7472d8d66ed3885bb93e573e51154d28333bd7
Luca <36957606+lucaoneto@users.noreply.github.com>
2020-09-01 16:57:44 +0200

    Update f_fai.m

--- f_fai.m
+++ f_fai.m
@@ -1,12 +1,4 @@
 function fai = f_fai(Y,S)
-%     uv = unique(Y)';
-%     fai = 0;
-%     for v = uv
-%         p0 = mean(Y(S==0) == v);
-%         p1 = mean(Y(S==1) == v);
-%         fai = fai + abs(p0-p1);
-%     end
-%     fai = fai/length(uv);
     tt = linspace(min(Y),max(Y),1000);
     vv = unique(S);
     nn = [sum(S==vv(1)),sum(S==vv(2))];
